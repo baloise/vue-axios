708a70ede780755538bd62ae7f66b30e93d2769b
hirsch <gery.hirschfeld@w3tec.ch>
2021-03-31 16:13:48 +0200

    chore(): update ci

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 export function isPromise(promise: any): boolean {
   return !!promise && typeof promise.then === 'function'
 }
